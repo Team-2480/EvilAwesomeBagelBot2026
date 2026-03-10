e2aa85005af7e11785d29ffd8e640d816b8cf66d
OLIVER427 <oliver427daminecraftplayer@gmail.com>
2026-03-10 18:29:07 -0500

    some things for dashboard

--- src/main/cpp/subsystems/Climb.cpp
+++ src/main/cpp/subsystems/Climb.cpp
@@ -1,11 +1,15 @@
 #include "Climb.h"
 
+#include "frc/smartdashboard/SmartDashboard.h"
+
 void ClimbSubsystem::Periodic() {
   if (climb_up) {
     climb_spool.SetControl(up_pos);
   } else {
     climb_spool.SetControl(down_pos);
   }
+  frc::SmartDashboard::PutBoolean("Climb Up", climb_up);
+  frc::SmartDashboard::PutNumber("Climb Motor Pos", climb_spool.GetPosition().GetValue().value());
 }
 
 void ClimbSubsystem::SetClimb(bool climb_set) { climb_up = climb_set; }
